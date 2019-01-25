a97c57de055671ffb2ac220f3d85abaa4cebf56d
Onwuka Gideon <dongidomed@gmail.com>
2019-01-25 20:58:34 +0100

    Initial commit

---
+++ assets/js/components/Login.vue
@@ -0,0 +1,66 @@
+
+<template>
+    <div class="login-container chat-box">
+        <div class="inputs">
+            <div class="" v-if="message"> {{message}} </div>
+            <input type="text" placeholder="Your name" v-model="name">
+            <input type="text" placeholder="Your email" v-model="email">
+            <button type="submit" class="submit" v-on:click="login"> Submit </button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+              name: "",
+              email: "",
+              message: ""
+            }
+        },
+        methods: {
+            login: function() {
+                // Do your real authentication here...
+                if ( !!(this.name && this.email) ) {
+                    this.$emit("authenticated", {email: this.email, name: this.name});
+                } else {
+                    this.message = "All inputs are required."
+                    // clear the message after 2 sec.
+                    setTimeout(() => {
+                        this.message = ""
+                    }, 2000);
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped> 
+    .login-container {
+        display: grid; 
+    }
+    input[type="text"] {
+        padding: 10px 8px;
+        margin-top: 10px;
+        border-radius: 2px;
+        border: 1px solid darkgray;
+        font-size: 16px;
+        box-sizing: border-box;
+        display: block;
+        width: 300px;
+    }
+    .inputs {
+        text-align: center;
+        align-self: center;
+        justify-self: center;
+    }
+    .submit {
+        width: 300px;
+        margin-top: 9px;
+        padding: 10px 60px;
+        background: rgb(99, 99, 212);
+        color: white;
+        font-size: 16px;
+    }
+</style>
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
+use Twilio\Jwt\AccessToken;
+use Twilio\Jwt\Grants\ChatGrant;
+
 class HomeController extends AbstractController
 {
      /**
@@ -15,4 +18,43 @@ class HomeController extends AbstractController
     {
         return $this->render('index.html.twig');
     }
+
+    /**
+     * @Route("/token", name="get_token", methods={"POST"})
+     */
+    public function authenticate( Request $request ) {
+
+        $identity = $request->request->get( 'email' );
+
+        // Required for all Twilio access tokens
+        $twilioAccountSid = getenv(  'ACCOUNT_SID' );
+        $twilioApiKey     = getenv(  'API_KEY' );
+        $twilioApiSecret  = getenv(  'API_SECRET' );
+
+        // Required for Chat grant
+        $serviceSid = getenv( 'SERVICE_INSTANCE_SID' );
+
+        // Create access token, which we will serialize and send to the client
+        $token = new AccessToken(
+            $twilioAccountSid,
+            $twilioApiKey,
+            $twilioApiSecret,
+            3600,
+            $identity
+        );
+
+        // Create Chat grant
+        $chatGrant = new ChatGrant();
+        $chatGrant->setServiceSid( $serviceSid);
+
+        // Add grant to token
+        $token->addGrant( $chatGrant );
+
+        // render token to json
+        return $this->json([
+            "status" => "success",
+            "token" => $token->toJWT()
+        ]);
+    }
+
 }
